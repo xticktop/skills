3ac5210f360716a5e704a954669ec767f54d3695
XTick <xticktop@163.com>
2026-03-19 09:58:49 +0800

    创建XTick-skills
    创建XTick-skills

--- xtick/scripts/Config.py
+++ xtick/scripts/Config.py
@@ -1,3 +1,3 @@
 class Config:
     SERVER_URL = "http://api.xtick.top"  # Api调用服务地址
-    TOKEN = "be9e09e3b9717d356c6fd64c8d861fd8"  # 登录XTick官网，获取token
+    TOKEN = "123456789"  # 登录XTick官网，获取token
